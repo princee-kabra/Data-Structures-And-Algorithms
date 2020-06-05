e5633c2c92a57965827ba80cc0d8096745b1d11a
princee-kabra <54720297+princee-kabra@users.noreply.github.com>
2020-06-05 16:21:37 +0530

    Add files via upload

---
+++ easy/rotate doubly linked list.cpp
@@ -0,0 +1,105 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+struct node
+{
+  int data;
+  struct node*next,*prev;
+  
+  node(int x){
+      data = x;
+      next = NULL;
+      prev = NULL;
+  }
+  
+};
+struct node* update(struct node*start,int p);
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,p;
+        cin>>n>>p;
+        struct node*start = NULL;
+        struct node* cur = NULL;
+        struct node* ptr = NULL;
+        
+        for(int i=0;i<n;i++)
+        {
+            int a;
+            cin>>a;
+            ptr=new node(a);
+            ptr->data=a;
+            ptr->next=NULL;
+            ptr->prev=NULL;
+            if(start==NULL)
+            {
+                start=ptr;
+                cur=ptr;
+            }
+            else
+            {
+                cur->next=ptr;
+                ptr->prev=cur;
+                cur=ptr;
+            }
+        }
+        struct node*str=update(start,p);
+        while(str!=NULL)
+        {
+            cout<<str->data<<" ";
+            str=str->next;
+        }
+        cout<<endl;  
+    }
+}
+
+ // } Driver Code Ends
+
+
+
+
+
+//User function Template for C++
+
+/*
+struct node
+{
+  int data;
+  struct node*next,*prev;
+  
+  node(int x){
+      data = x;
+      next = NULL;
+      prev = NULL;
+  }
+  
+};
+*/
+
+struct node*update(struct node*start,int p)
+{    node *ptr=start;
+node*q=start;
+    for(int i=0;i<p;i++)
+    {   q=ptr;
+        ptr=ptr->next;
+    }
+    node*temp=ptr;
+while(temp->next!=NULL)
+{
+    temp=temp->next;
+}
+  temp->next=start;
+  start->prev=temp;
+  q->next=NULL;
+  ptr->prev=NULL;
+ return ptr;
+}
+
+
+// { Driver Code Starts.
+  // } Driver Code Ends
